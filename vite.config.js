0d992e922db099997115955b1a099b50c47e17c0
Rakhesh <rakhesh@example.com>
2026-05-14 11:52:21 +0530

    Configure project for AWS Amplify deployment

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
-  build: {
-    outDir: 'public_html',
-  },
+  base: '/',
 })
